dc4ba6f11a80bf7812b273afd2d3725cec027ea7
Averizer <36316073+Averizer@users.noreply.github.com>
2020-02-13 10:43:36 -0600

    Update practica0.py
    sdxfsdf

--- practica0.py
+++ practica0.py
@@ -1,6 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def sumar(xvector1,yvector1,xvector2,yvector2):
+    v1 = len(xvector1)
+    v2 = len(xvector2)
+    vxResultante = []
+    vyResultante = []
+
+    if v1 > v2:
+        n = v1 - v2
+        for i in range(n):
+            xvector2.append(0)
+
+        for i in range(v1):
+            vxResultante.append(xvector1[i]+xvector2[i])
+            vyResultante.append(yvector1[i]+yvector2[i])
+        plt.scatter(vxResultante, vyResultante, label = ('Clase resultante'))
+        plt.show()
+
+    
+    #if v2 > v1:
+    
+    #else:
+
+
 print("        Hola bienvenid@ a la practica 0")
 print("Para comenzar ingresa el numero de clases que habrá.")
 n = int(input("\tNumero = "))
@@ -39,8 +62,8 @@ while c < n:
         xaxis.append(classes[c][x][0])
         yaxis.append(classes[c][x][1])
     plt.scatter(xaxis, yaxis, label = ('Clase C'+ str(c)))
-    print(xaxis)
-    print(yaxis)
+    x_per_class.append(xaxis)
+    y_per_class.append(yaxis)
     xaxis = []
     yaxis = []
     c = c+1
@@ -51,45 +74,27 @@ plt.ylabel("Característica y")
 plt.legend()
 plt.show()
 op = 0
+print ("x por clase:")
+print(x_per_class)
 while op != 4:
+
     print("\nAhora que tienes los vectores podemos trabajar con ellos"
          +"\n Digita la opción que desees realizar.")
     print("1.- Sumar")
-    print("2.-Restar")
+    print("2.- Restar")
     print("3.- Multiplicar")
     print("4.- Salir")
     op = input("Ingresa la opción: ")
-    if op == 1:
-        v1 = input("Selecciona el primer vector a sumar Ci: ")
-        v2 = print("Selecciona el segundo vector Ci: ")
+    if op == "1":
+        v1 = int(input("Selecciona el primer vector a sumar Ci: "))
+        v2 = int(input("Selecciona el segundo vector Ci: "))
         sumar(x_per_class[v1],y_per_class[v1],x_per_class[v2],y_per_class[v2])
-    if op == 4:
+    if op == "4":
         break
 
 
 
 
 
-def sumar(xvector1,yvector1,xvector2,yvector2):
-    v1 = len(xvector1)
-    v2 = len(xvector2)
-    vxResultante = []
-    vyResultante = []
-
-    if v1 > v2:
-        n = v1 - v2
-        for i in range(n):
-            xvector2.append(0)
-
-        for i in range(v1):
-            vxResultante.append(xvector1[i]+xvector2[i])
-            vyResultante.append(yvector1[i]+yvector2[i])
-        plt.scatter(vxResultante, vyResultante, label = ('Clase resultante'))
-        plt.show()
-
-    
-    #if v2 > v1:
-    
-    #else:
 
 
