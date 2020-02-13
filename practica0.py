162429eaa06bb91aa5c0b26e1630506b393220a0
Averizer <36316073+Averizer@users.noreply.github.com>
2020-02-13 00:31:33 -0600

    Update practica0.py
    sdfsdf

--- practica0.py
+++ practica0.py
@@ -15,7 +15,7 @@ print("\n\nAhora que tenemos las clases procederemos a llenarlas con las caracte
 ft_class = []
 classes = []
 c = 0
-
+#--------------------Llenar clases-------------------------------------
 while c < n:
     c2 = n_por_clase[c]
     print("\nClase a registrar características C"+ str(c))
@@ -30,6 +30,9 @@ print(classes)
 c=0 
 xaxis = []
 yaxis = []
+x_per_class =[]
+y_per_class =[]
+#---------------------Imprimir clases---------------------------------
 while c < n:
     c2 = n_por_clase[c]
     for x in range(c2):
@@ -47,3 +50,46 @@ plt.xlabel("Característica x")
 plt.ylabel("Característica y")
 plt.legend()
 plt.show()
+op = 0
+while op != 4:
+    print("\nAhora que tienes los vectores podemos trabajar con ellos"
+         +"\n Digita la opción que desees realizar.")
+    print("1.- Sumar")
+    print("2.-Restar")
+    print("3.- Multiplicar")
+    print("4.- Salir")
+    op = input("Ingresa la opción: ")
+    if op == 1:
+        v1 = input("Selecciona el primer vector a sumar Ci: ")
+        v2 = print("Selecciona el segundo vector Ci: ")
+        sumar(x_per_class[v1],y_per_class[v1],x_per_class[v2],y_per_class[v2])
+    if op == 4:
+        break
+
+
+
+
+
+def sumar(xvector1,yvector1,xvector2,yvector2):
+    v1 = len(xvector1)
+    v2 = len(xvector2)
+    vxResultante = []
+    vyResultante = []
+
+    if v1 > v2:
+        n = v1 - v2
+        for i in range(n):
+            xvector2.append(0)
+
+        for i in range(v1):
+            vxResultante.append(xvector1[i]+xvector2[i])
+            vyResultante.append(yvector1[i]+yvector2[i])
+        plt.scatter(vxResultante, vyResultante, label = ('Clase resultante'))
+        plt.show()
+
+    
+    #if v2 > v1:
+    
+    #else:
+
+
